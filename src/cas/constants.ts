58669462740620a164fab6fa9cc50e373972f83f
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-12-14 16:31:28 +0100

    fix(cas): move signatures to external clients

--- src/cas/constants.ts
+++ src/cas/constants.ts
@@ -4,8 +4,6 @@ export const HOST = "https://cas.unilim.fr";
 
 export const EXTERNAL_SERVICES = {
   IUT_COMMUNITY_MOODLE: "https://community-iut.unilim.fr/login/index.php?authCAS=CAS",
-  IUT_SIGNATURES: "https://signatures.unilim.fr/",
-
   COMMUNITIES_MOODLE: "https://communities.unilim.fr/login/index.php?authCAS=CAS",
   GRADING: "https://inscription.unilim.fr/gestion/etudiant/rvn/"
 } as const;
@@ -15,6 +13,12 @@ export const EXTERNAL_CLIENTS = {
     id: "biome-prod",
     scopes: ["openid", "profile", "email"],
     redirectionURL: "https://biome.unilim.fr/authentication/callback"
+  } satisfies ExternalClient,
+
+  IUT_SIGNATURES: {
+    id: "signatures",
+    scopes: ["openid", "profile"],
+    redirectionURL: "https://signatures.unilim.fr/callback"
   } satisfies ExternalClient
 } as const;
 
