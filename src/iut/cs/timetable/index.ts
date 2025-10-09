5a0eabc41ed1596c32dd3d35ea18db258a301093
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-10-09 08:30:17 +0200

    feat(timetable): add years export

--- src/iut/cs/timetable/index.ts
+++ src/iut/cs/timetable/index.ts
@@ -1,8 +1,7 @@
+export { TimetableYear } from "./models/TimetableYear";
 export * from "./online/OnlineTimetable";
 export * from "./online/OnlineTimetableFileEntry";
-
 export { getTimetableFromBuffer } from "./parser";
 export type { Timetable } from "./parser";
-
 export { LESSON_TYPES, SUBGROUPS } from "./parser/constants";
 export type { TimetableLesson, TimetableLessonCM, TimetableLessonDS, TimetableLessonOTHER, TimetableLessonSAE, TimetableLessonTD, TimetableLessonTP } from "./parser/lessons";
